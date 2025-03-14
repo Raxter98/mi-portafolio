a45f916ba9ea9ccb55b3a779885f663660b6743b
Marcos <soymarcos354@gmail.com>
2025-03-14 16:21:05 -0300

    agregando modificaciones a la navar

--- components/nav/page.tsx
+++ components/nav/page.tsx
@@ -32,68 +32,9 @@ export default function Navbar(){
 
 
     return (
-      <div >
-        {toggle ? (
-          <div className=" md:mt-5 z-50 fixed md:inline-flex  md:h-16 md:justify-center md:p-4 md:items-center  md:bg-[#00000005] md:rounded-lg md:shadow-lg ">
-            <ul>
-            <li className="md:hidden ">
-              <div className="md:hidden  text-white hover:text-[#1a1a1a] ml-2">
-                <button className="md:hidden " onClick={() => hidden()}>
-                  <AplicacionIcon />
-                </button>
-              </div>
-            </li>
-           
-            <div className="md:inline-flex ">
-              <li className="text-white  transition transform  md:hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none font-[family-name:var(--font-geist-mono)] rounded md:mr-12 hover:text-black hover:bg-gray-50 md:p-1 p-1">
-                <button onClick={() => scrollView("home")}>
-                  <span className="block hover:translate-x-4 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none md:hover:translate-x-0">
-                    Home
-                  </span>
-                </button>
-              </li>
-              <li className="text-white transition transform  md:hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none font-[family-name:var(--font-geist-mono)] rounded md:mr-12 hover:text-black hover:bg-gray-50 md:p-1 p-1">
-                <button onClick={() => scrollView("about")}>
-                  <span className="block hover:translate-x-4 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none md:hover:translate-x-0">
-                    About me
-                  </span>
-                </button>
-              </li>
-              <li className="text-white transition transform md:hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none font-[family-name:var(--font-geist-mono)] rounded md:mr-12 hover:text-black hover:bg-gray-50 md:p-1 p-1">
-                <button onClick={() => scrollView("projects")}>
-                  <span className="block hover:translate-x-4 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none md:hover:translate-x-0">
-                    My projects
-                  </span>
-                </button>
-              </li>
-              <li className="text-white transition transform md:hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none font-[family-name:var(--font-geist-mono)] rounded md:mr-12 hover:text-black hover:bg-gray-50 md:p-1 p-1">
-                <button onClick={() => scrollView("skills")}>
-                  <span className="block hover:translate-x-4 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none md:hover:translate-x-0">
-                    Skills
-                  </span>
-                </button>
-              </li>
-              <li className="text-white transition transform md:hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none font-[family-name:var(--font-geist-mono)] rounded md:mr-12 hover:text-black hover:bg-gray-50 md:p-1 p-1">
-                <button onClick={() => scrollView("contact")}>
-                  <span className="block hover:translate-x-4 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none md:hover:translate-x-0">
-                    Contact me
-                  </span>
-                </button>
-              </li>
-            </div>
-            </ul>
-          </div>
-        ) : (
-            <div className=" md:mt-5 z-50 fixed md:inline-flex  md:h-16 md:justify-center md:p-4 md:items-center  md:bg-[#00000005] md:rounded-lg md:shadow-lg ">
-            <div className="md:hidden  text-white hover:text-[#1a1a1a] ml-2">
-              <button className="md:hidden " onClick={() => hidden()}>
-                <AplicacionIcon />
-              </button>
-            </div>
-
-            
-            <div className="md:inline-flex ">
-              <ul className="hidden md:inline-flex md:justify-end w-full  ">
+      <div>
+        <div className="hidden md:flex mt-4">
+              <ul className="md:inline-flex    ">
                 <li className="text-white transition transform  md:hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none font-[family-name:var(--font-geist-mono)] rounded md:mr-12 hover:text-black hover:bg-gray-50 md:p-1 p-1">
                   <button onClick={() => scrollView("home")}>
                     <span className="block hover:translate-x-4 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none md:hover:translate-x-0">
@@ -131,9 +72,60 @@ export default function Navbar(){
                   </button>
                 </li>
               </ul>
-            </div>
           </div>
-        )}
+          
+      
+
+
+
+
+          <div className="md:hidden fixed z-50">
+<div className=" text-white hover:text-[#1a1a1a]">
+<button onClick={()=>hidden()}>
+
+          <AplicacionIcon/>
+</button>
+</div>
+              <ul className={toggle ? "md:inline-flex": "hidden"}>
+                <li className="text-white transition transform  md:hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none font-[family-name:var(--font-geist-mono)] rounded md:mr-12 hover:text-black hover:bg-gray-50 md:p-1 p-1">
+                  <button onClick={() => scrollView("home")}>
+                    <span className="block hover:translate-x-4 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none md:hover:translate-x-0">
+                      Home
+                    </span>
+                  </button>
+                </li>
+
+                <li className="text-white transition transform md:hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none font-[family-name:var(--font-geist-mono)] rounded md:mr-12 hover:text-black hover:bg-gray-50 md:p-1 p-1">
+                  <button onClick={() => scrollView("about")}>
+                    <span className="block hover:translate-x-4 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none md:hover:translate-x-0">
+                      About me
+                    </span>
+                  </button>
+                </li>
+                <li className="text-white transition transform md:hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none font-[family-name:var(--font-geist-mono)] rounded md:mr-12 hover:text-black hover:bg-gray-50 md:p-1 p-1">
+                  <button onClick={() => scrollView("projects")}>
+                    <span className="block hover:translate-x-4 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none md:hover:translate-x-0">
+                      My projects
+                    </span>
+                  </button>
+                </li>
+                <li className="text-white transition transform md:hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none font-[family-name:var(--font-geist-mono)] rounded md:mr-12 hover:text-black hover:bg-gray-50 md:p-1 p-1">
+                  <button onClick={() => scrollView("skills")}>
+                    <span className="block hover:translate-x-4 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none md:hover:translate-x-0">
+                      Skills
+                    </span>
+                  </button>
+                </li>
+                <li className="text-white transition transform md:hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none font-[family-name:var(--font-geist-mono)] rounded md:mr-12 hover:text-black hover:bg-gray-50 md:p-1 p-1">
+                  <button onClick={() => scrollView("contact")}>
+                    <span className="block hover:translate-x-4 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none md:hover:translate-x-0">
+                      Contact me
+                    </span>
+                  </button>
+                </li>
+              </ul>
+          </div>
+          
       </div>
     );
 }
